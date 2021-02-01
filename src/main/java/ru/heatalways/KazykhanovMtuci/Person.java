38a72ae8cfcf44a9e7703afd912364dd7a7e71db
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-02-01 11:09:07 +0400

    Создание класса личности

---
+++ src/main/java/ru/heatalways/KazykhanovMtuci/Person.java
@@ -0,0 +1,27 @@
+package ru.heatalways.KazykhanovMtuci;
+
+public class Person {
+    private int age;
+    private String firstName;
+
+    public Person(int age, String firstName) {
+        this.age = age;
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
